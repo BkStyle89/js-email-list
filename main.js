2f626ed01d765a990a86b9a5dbc9cf77694725a8
BkStyle89 <kajzer@hotmail.it>
2026-02-23 16:44:08 +0100

    one page list

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
 /* https://flynn.boolean.careers/exercises/api/random/mail */
 console.log("test");
-const listLiEl =document.querySelector(".list-group-item")
+const listUlEl =document.querySelector(".list-group")
+const refreshButtonEl=document.querySelector(".refresh")
 
-console.log(listLiEl);
+console.log(listUlEl);
 
+
+let mail =[]
 
 for(let i=0; i< 10;i++){
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
     .then(response => response.json())
     .then(mails =>{
         let multipleMails = mails.response
-        console.log(multipleMails);
-   
-    
+   listUlEl.innerHTML += `<li> ${multipleMails} </li>`
+   mail.push(multipleMails)  
 }
 )}
+
+console.log(mail);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//test
+
+
+/* const listEl=document.getElementById("list") */
+/* console.log(listLiEl); */
+/* console.log(multipleMails); */
+/* listEl.innerText=multipleMails */
+/* listEl.innerHTML= mail */
+
+/* for(let i=0; i<mail.length;i++){
+const email = mail.length[i]
+console.log(email);
+listLiEl.innerHTML=email
+} */
